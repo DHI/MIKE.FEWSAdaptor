a8220af864d86f87423d6616ee0bde0a1ad8f49c
Standa Vanecek <SV@dhigroup.com>
2020-12-01 12:27:37 +0000

    new readme

---
+++ BuildZip_powershell.bat
@@ -0,0 +1,9 @@
+set zipDir=testdata\config\ModuleDataSetFiles
+IF NOT EXIST %zipDir% MKDIR %zipDir%
+del /Q %zipDir%\*.*
+
+powershell.exe Compress-Archive bin\dfs0ToPI\*.* '%zipDir%\importDfs0 Default.zip'     
+powershell.exe Compress-Archive bin\ModifyMhydroFile\*.* '%zipDir%\modify_and_run Default.zip'
+powershell.exe Compress-Archive bin\MHydro\*.* '%zipDir%\modify_py Default.zip'
+powershell.exe Compress-Archive bin\PITodfs0\*.* '%zipDir%\PI2Dfs0 Default.zip'
+powershell.exe Compress-Archive bin\Res1DToPI\*.* '%zipDir%\Res1d2PI Default.zip' 
